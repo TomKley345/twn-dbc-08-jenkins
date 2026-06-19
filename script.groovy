8886df3b784acea435cad2c81ccf70ba531a76a2
Tom Kley <tk345@gmx.de>
2026-06-19 08:49:56 +0200

    ADD source  labels in script.groovy echo statements

--- script.groovy
+++ script.groovy
@@ -1,10 +1,10 @@
 def buildJar() {
-    echo 'building the application...'
+    echo 'script.groovy: building the application ...'
     sh 'mvn package'
 }
 
 def buildImage() {
-    echo "building the docker image..."
+    echo "script.grrovy: building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         sh 'docker build -t tomkley/demo-app:jma-2.1 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
